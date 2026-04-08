691f00152d84c9e814cac4500f68284a2db144b1
Casper <caspervgameren@outlook.com>
2026-04-08 13:46:36 +0200

    ADDED a quick onclick function for the modal I will soon implement

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -2,25 +2,25 @@ import { Skills } from "../types/skills";
 
 export const skills: Skills = {
     languages: [
-        { name: "HTML", icon: "logos:html-5", href: ""},
-        { name: "CSS", icon: "logos:css-3", href: ""},
-        { name: "SCSS", icon: "logos:sass", href: ""},
-        { name: "JavaScript", icon: "logos:javascript", href: ""},
-        { name: "TypeScript", icon: "logos:typescript-icon", href: ""},
-        { name: "PHP", icon: "logos:php", href: ""},
+        { name: "HTML", icon: "logos:html-5",},
+        { name: "CSS", icon: "logos:css-3",},
+        { name: "SCSS", icon: "logos:sass",},
+        { name: "JavaScript", icon: "logos:javascript",},
+        { name: "TypeScript", icon: "logos:typescript-icon",},
+        { name: "PHP", icon: "logos:php",},
     ],
     frameworks: [
-        { name: "SolidJS", icon: "logos:solidjs-icon", href: ""},
-        { name: "React", icon: "logos:react", href: ""},
-        { name: "Svelte", icon: "logos:svelte-icon", href: ""},
-        { name: "Laravel", icon: "logos:laravel", href: ""},
-        { name: "Tailwind CSS", icon: "logos:tailwindcss-icon", href: ""},
+        { name: "SolidJS", icon: "logos:solidjs-icon",},
+        { name: "React", icon: "logos:react",},
+        { name: "Svelte", icon: "logos:svelte-icon",},
+        { name: "Laravel", icon: "logos:laravel",},
+        { name: "Tailwind CSS", icon: "logos:tailwindcss-icon",},
     ],
     tools: [
-        { name: "Visual Studio Code", icon: "logos:visual-studio-code", href: ""},
-        { name: "PHPStorm", icon: "logos:phpstorm", href: ""},
-        { name: "Git", icon: "logos:git-icon", href: ""},
-        { name: "Github", icon: "logos:github-icon", href: ""},
-        { name: "Figma", icon: "logos:figma", href: ""},
+        { name: "Visual Studio Code", icon: "logos:visual-studio-code",},
+        { name: "PHPStorm", icon: "logos:phpstorm",},
+        { name: "Git", icon: "logos:git-icon",},
+        { name: "Github", icon: "logos:github-icon", href: "https://github.com/CaspervGameren"},
+        { name: "Figma", icon: "logos:figma",},
     ]
 }
