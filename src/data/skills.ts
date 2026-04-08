be713284ccfeec9ca351cb4def81629e7e03fad4
Casper <caspervgameren@outlook.com>
2026-04-08 13:35:37 +0200

    REMOVED all svg scince they are not being used UPDATED types skills to interfaces

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -2,20 +2,25 @@ import { Skills } from "../types/skills";
 
 export const skills: Skills = {
     languages: [
-        { name: "HTML", icon: "logos:html-5",},
-        { name: "CSS", icon: "logos:css-3",},
-        { name: "SCSS", icon: "logos:sass",},
-        { name: "JavaScript", icon: "logos:javascript",},
-        { name: "TypeScript", icon: "logos:typescript-icon",},
-        { name: "PHP", icon: "logos:php",},
+        { name: "HTML", icon: "logos:html-5", href: ""},
+        { name: "CSS", icon: "logos:css-3", href: ""},
+        { name: "SCSS", icon: "logos:sass", href: ""},
+        { name: "JavaScript", icon: "logos:javascript", href: ""},
+        { name: "TypeScript", icon: "logos:typescript-icon", href: ""},
+        { name: "PHP", icon: "logos:php", href: ""},
     ],
     frameworks: [
-        { name: "SolidJS", icon: "logos:solidjs-icon",},
-        { name: "Tailwind CSS", icon: "logos:tailwindcss-icon",},
+        { name: "SolidJS", icon: "logos:solidjs-icon", href: ""},
+        { name: "React", icon: "logos:react", href: ""},
+        { name: "Svelte", icon: "logos:svelte-icon", href: ""},
+        { name: "Laravel", icon: "logos:laravel", href: ""},
+        { name: "Tailwind CSS", icon: "logos:tailwindcss-icon", href: ""},
     ],
     tools: [
-        { name: "PHPStorm", icon: "logos:visual-studio-code",},
-        { name: "Visual Studio Code", icon: "logos:phpstorm",},
-        { name: "Figma", icon: "logos:figma",},
+        { name: "Visual Studio Code", icon: "logos:visual-studio-code", href: ""},
+        { name: "PHPStorm", icon: "logos:phpstorm", href: ""},
+        { name: "Git", icon: "logos:git-icon", href: ""},
+        { name: "Github", icon: "logos:github-icon", href: ""},
+        { name: "Figma", icon: "logos:figma", href: ""},
     ]
 }
--- src/types/skills.ts
+++ src/types/skills.ts
@@ -1,9 +1,17 @@
-export type Skill = {
+export interface Skill {
     name: string,
     icon: string,
+    href?: string,
+    details?: SkillDetails,
 }
 
-export type Skills = {
+export interface SkillDetails {
+    description: string,
+    experience: string,
+    projects?: string[],
+}
+
+export interface Skills {
     languages: Skill[],
     frameworks: Skill[],
     tools: Skill[],
