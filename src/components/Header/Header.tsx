82421c2dcb006d070174dcf46dad4fcfe41bf4d3
Casper <caspervgameren@outlook.com>
2026-07-05 14:31:25 +0200

    UPDATED fixed brand color utils in nav AND header text color

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ export const Header: Component = () => {
 
     return (
         <header class="grid grid-cols-[auto_auto] gap-12 justify-center h-[600px]">
-            <h1>Software Developer</h1>
+            <h1 class="text-6xl brand-gradient-text font-bolder">Software Developer</h1>
             <img src="/images/portfolio-image.png" alt="Foto van mijn gezicht bewerkt met AI" class="justify-self-start" />
         </header>
     );
