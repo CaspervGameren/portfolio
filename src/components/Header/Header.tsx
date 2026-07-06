5181dd4b0ff156f9d60d365b8aed33ec9e8aa668
Casper <caspervgameren@outlook.com>
2026-07-06 20:49:54 +0200

    UPDATED projects secton to read project title AND updated application title to Casper - Portfolio

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Component } from "solid-js";
 export const Header: Component = () => {
 
     return (
-        <header class="grid grid-cols-[auto_auto] gap-12 justify-center h-[600px]">
+        <header class="grid grid-cols-[auto_auto] gap-12 justify-center h-[600px] mb-50">
             <h1>Software Developer</h1>
             <img src="/images/portfolio-image.png" alt="Foto van mijn gezicht bewerkt met AI" class="justify-self-start" />
         </header>
