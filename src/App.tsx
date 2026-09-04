840290b916d0934910f5ecaf98a76ffce1f1c9f7
Casper <caspervgameren@outlook.com>
2026-09-04 16:38:32 +0200

    ADDED gsap

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Nav, Header, Projects } from './components';
 const App: Component = () => {
   return (
     <>
-      <main class='bg-[#222222] min-h-screen w-full flex flex-col items-center text-[#f1f1f1]'>
+      <main class=''>
         <Nav />
         <Header />
         <Projects />
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,19 +4,19 @@ const NAV_ITEMS = ["Home", "Projects", "About", "Techstack", "Contact"];
 
 export const Nav: Component = () => {
     return (
-        <nav class="w-full flex items-center justify-between h-[80px] px-8">
-            <a href="#" data-text="CvG" class="text-6xl brand-gradient-text hover:[--gradient-opacity:1]">CvG</a>
-            <ul class="flex items-center px-4 gap-x-2 h-[60px] rounded-md bg-amber-500 backdrop-blur-md border border-brand-gradient shadow-lg">
+        <nav class="grid grid-cols-3 w-full h-20 items-center jusitfy-center px-6 border-b">
+            <a href="#" data-text="CvG" class="">CvG</a>
+            <ul class="flex justify-between">
                 <For each={NAV_ITEMS}>
                     {(item) => (
-                        <li class="cursor-pointer px-4 py-2 rounded-lg hover:brand-gradient bg-(--section-bg) text-white/90 hover:text-black font-bold">
+                        <li class="cursor-pointer">
                             {item}
                         </li>
                     )}
                 </For>
             </ul>
 
-            <a href="#contact" class="flex items-center hover:brand-gradient px-4 py-2 h-[60px] rounded-xl text-white hover:text-black font-bold  bg-(--section-bg)">
+            <a href="#contact" class="justify-self-end">
                 Get in touch
             </a>
         </nav>
