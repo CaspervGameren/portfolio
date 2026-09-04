2a703a82cd7ed4562d8f887e075d9c27ba9a4a57
Casper <caspervgameren@outlook.com>
2026-09-04 16:52:47 +0200

    UPDATED header to contain basic screen idea for Learning like an LLM text

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './index.css';
 import type { Component } from 'solid-js';
 import { Nav, Header, Projects } from './components';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 const App: Component = () => {
   return (
     <>
-      <main class=''>
+      <main class='bg-gray-900 text-white h-dvh'>
         <Nav />
         <Header />
-        <Projects />
+        {/* <Projects /> */}
       </main>
     </>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,8 +3,13 @@ import { Component } from "solid-js";
 export const Header: Component = () => {
     return (
         <header >
-            <div></div>
-            <div></div>
+            <div class="flex mx-auto my-40 min-h40 min-w-40 h-50 w-fit items-center bg-white">
+                <h1 class="flex flex-col items-center text-5xl font-bold text-black py-12 px-20">
+                    <span><strong>Learning</strong></span>
+                    <span>Like an</span>
+                    <span><strong>LLM</strong></span>
+                </h1>
+            </div>
         </header>
     );
 };
